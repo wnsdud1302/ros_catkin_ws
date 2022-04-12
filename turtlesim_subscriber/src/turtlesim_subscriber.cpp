3b7976ea335f0e31b36f32ab318754e8ba5a4717
wnsdud1302 <wnsdud1302@naver.com>
2022-04-12 15:40:44 +0900

    과제1

--- turtlesim_subscriber/src/turtlesim_subscriber.cpp
+++ turtlesim_subscriber/src/turtlesim_subscriber.cpp
@@ -2,16 +2,23 @@
 #include <turtlesim/Pose.h>
 
 
-void poseCallBack(const turtlesim::Pose::ConstPtr& msg){
-    ROS_INFO("pose: %0.1f, %0.1f, %0.1f ", msg->x, msg->y, msg->theta); 
+void poseCallBack(const turtlesim::PoseConstPtr& msg){
+    ROS_INFO("pose: x:%0.1f, y:%0.1f, theta:%0.1f, linear_velocity:%0.1f,  ngular_velocity:%0.1f"
+        , msg->x, msg->y, msg->theta, msg->linear_velocity, msg->angular_velocity); 
 }
 
 int main(int argc, char **argv){
-    ros::init(argc, argv, "turtlesim subscriber");
+    ros::init(argc, argv, "turtlesim_subscriber");
     ros::NodeHandle nh;
-    ros::Subscriber pose_sub = nh.subscribe("tutle1/pose", 1, poseCallBack);
+    ros::Subscriber pose_sub = nh.subscribe("/turtle1/pose", 1, poseCallBack);
 
-    ros::spin();
+    ros::Rate loop_rate(10);
+
+    while(ros::ok()){
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    //ros::spin();
 
     return 0;
 }
